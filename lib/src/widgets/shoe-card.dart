cf17b88a9ee24d2d732b62549daa2333756c01ae
Yoni Calsin <yonicb20@gmail.com>
2020-08-01 23:56:21 -0500

    Created the shoes sizes card, widget

--- lib/src/widgets/shoe-card.dart
+++ lib/src/widgets/shoe-card.dart
@@ -17,9 +17,63 @@ class ShoeCard extends StatelessWidget {
         child: Column(
           children: <Widget>[
             _ShoeWithShadow(),
+            _ShoeSizes(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _ShoeSizes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          _ShoeSizesBox(7),
+          _ShoeSizesBox(7.5),
+          _ShoeSizesBox(8),
+          _ShoeSizesBox(8.5),
+          _ShoeSizesBox(9),
+        ],
+      ),
+    );
+  }
+}
+
+class _ShoeSizesBox extends StatelessWidget {
+  final double label;
+  const _ShoeSizesBox(this.label);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+        color: (this.label == 9) ? Color(0xffF1A23A) : Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          if (this.label == 9)
+            BoxShadow(
+              color: Color(0xffF1A23A),
+              blurRadius: 10,
+              offset: Offset(0, 5),
+            )
+        ],
+      ),
+      child: Text(
+        "${label.toString().replaceAll(".0", "")}",
+        style: TextStyle(
+          color: (this.label == 9) ? Colors.white : Color(0xffF1a23a),
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      alignment: Alignment.center,
     );
   }
 }
